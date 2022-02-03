eb71b3ec54efb72edad1630c7265b9783d6d610e
Eric Matlock <ermatlock@gmail.com>
2022-02-02 22:28:52 -0500

    Create returnCurrentCard and takeTurn methods

---
+++ src/Round.js
@@ -0,0 +1,27 @@
+const Turn = require("./Turn");
+
+class Round {
+	constructor(deck) {
+		this.deck = deck;
+		this.turn = 0;
+		this.incorrectGuesses = [];
+		this.newTurn = {}
+
+	}
+
+	returnCurrentCard() {
+		return this.deck.cards[0];
+	}
+
+	takeTurn(guess) {
+		this.newTurn = new Turn(guess, this.deck.cards[0]);
+		if (!this.newTurn.evaluateGuess()) {
+			this.incorrectGuesses.push(this.deck.cards[0].id)
+		}
+		this.turn++;
+		this.deck.cards.shift();
+		return this.newTurn.giveFeedback();
+	}
+}
+
+module.exports = Round;
