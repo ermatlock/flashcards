7118504636ee11f254f2b1455d8a2d0be1cc70ef
Eric Matlock <ermatlock@gmail.com>
2022-02-03 20:51:45 -0500

    Create start test

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,6 +1,10 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const Card = require("../src/Card");
+const Turn = require("../src/Turn");
+const Deck = require("../src/Deck");
+const Round = require("../src/Round");
 const Game = require("../src/Game");
 
 describe("Game", () => {
@@ -12,11 +16,20 @@ describe("Game", () => {
 		expect(Game).to.be.a("function");
 	});
 
-	it("should be an instance of Round", () => {
+	it("should be an instance of Game", () => {
 		expect(game).to.be.an.instanceof(Game);
 	});
 
 	it("should store the current round", () => {
 		expect(game.currentRound).to.eql({});
+	});
+
+	it("should start game with a round instance with a deck of cards", () => {
+		game.start();
+		const round = game.currentRound;
+
+		expect(round).to.be.an.instanceof(Round);
+		expect(round.deck).to.be.an.instanceof(Deck);
+		expect(round.deck.cards.length).to.eql(30);
 	});
 });
