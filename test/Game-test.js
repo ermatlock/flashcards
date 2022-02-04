5e0b8d92de0a71738e91c57de8aa55e093fd9806
Eric Matlock <ermatlock@gmail.com>
2022-02-03 19:15:05 -0500

    Create class instance test

---
+++ test/Game-test.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Game = require("../src/Game");
+
+describe("Game", () => {
+	beforeEach(() => {
+		game = new Game();
+	});
+
+	it("should be a function", () => {
+		expect(Game).to.be.a("function");
+	});
+
+	it("should be an instance of Round", () => {
+		expect(game).to.be.an.instanceof(Game);
+	});
+
+	
+});
