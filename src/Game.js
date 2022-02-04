da7c6e7ac91d13bfca6df2b53fc6626eddb89a3c
Eric Matlock <ermatlock@gmail.com>
2022-02-03 20:54:58 -0500

    Create currentRound property

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,23 @@
-const data = require('./data');
+const data = require("./data");
 const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const util = require("./util");
+const Card = require("./Card");
+const Deck = require("./Deck");
+const Round = require("./Round");
 
 class Game {
-  constructor() {}
+	constructor() {
+		this.currentRound = {};
+	}
 
-  printMessage(deck, round) {
-    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
-  }
+	printMessage(deck, round) {
+		console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+-----------------------------------------------------------------------`);
+	}
 
-  printQuestion(round) {
-      util.main(round);
-  }
+	printQuestion(round) {
+		util.main(round);
+	}
 }
 
 module.exports = Game;
