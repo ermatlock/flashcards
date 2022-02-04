ea13d77e27643a00c9e765ea0872ceb410bd6687
Eric Matlock <ermatlock@gmail.com>
2022-02-03 20:57:33 -0500

    Complete start method

--- src/Game.js
+++ src/Game.js
@@ -12,7 +12,10 @@ class Game {
 
   start() {
 		const cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer));
-
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
 	printMessage(deck, round) {
